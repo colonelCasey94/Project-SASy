35b03fd320bbb7b0af902c6c0cafe0aab183864a
colonelCasey94 <jadorer@email.arizona.edu>
2014-10-05 14:52:19 -0700

    setup initial look for program

--- ProjectSasy/ProjectSasy.ino
+++ ProjectSasy/ProjectSasy.ino
@@ -1,1 +1,36 @@
-//Hello Friend
+/*
+  Project SASy
+  Reads an analog input from a microphone and uses that input over time to calculate if a group of students
+  is being too lound in which case the ATMEGA will flash a stobe.
+  
+  Code:
+  Jacob Dorer
+  Hannah Ingle
+  
+  Hardware:
+  John "Jack" Gentery
+  Rachel Powers
+  (unknown)
+*/
+
+void input(float voltages[]);
+float average(float voltages[]);
+
+float averages[1000] = 0;
+
+//sets up LCD output and analog input
+void setup(){
+  
+}
+
+void loop(){
+  
+}
+
+void input(float voltages[]){
+  
+}
+
+float average(float voltages[]){
+  
+}
