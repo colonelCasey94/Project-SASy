39fe76d8ed8b9a2b10f8fcb07796a5380ec5755b
colonelCasey94 <jadorer@email.arizona.edu>
2014-11-02 15:10:47 -0700

    no message

--- ProjectSasy/ProjectSasy.ino
+++ ProjectSasy/ProjectSasy.ino
@@ -20,42 +20,45 @@
 //LCD_RS pint to digital pin 12
 //LCD_E pin to digital pin 11
 //LCD_D(4:7) pins to digital pins (5:2)
-LiquidCrystal lcd (12,11,5,4,3,2);
+//LiquidCrystal lcd (12,11,5,4,3,2);
 
 //creates input array
 void input(float voltages[]);
 //calculates average of voltages
 float average(float voltages[]);
 
-<<<<<<< HEAD
 //stores the voltage input 
 float inputs[1000];
 float inputRef = 2.5;
-=======
-float averages[1000];
->>>>>>> origin/hannah
 
 //sets up LCD output and analog input
 void setup(){
-  lcd.begin(16, 2);
-  lcd.print("hello, world!");
+//  lcd.begin(16, 2);
+//  lcd.print("hello, world!");
   delay(3000);
+  Serial.begin(9600);
+  Serial.print("hello");
 }
 
 void loop(){
   input(inputs);
-  lcd.setCursor(0,1);
-  lcd.print("voltage = ");
+//  lcd.setCursor(0,1);
+//  lcd.print("voltage = ");
   int ones = (int)average(inputs)%10;
   int decimals = (int)(average(inputs)*100)%100;
-  lcd.print(ones);
-  lcd.print(".");
-  lcd.print(decimals);  
+//  lcd.print(ones);
+//  lcd.print(".");
+//  lcd.print(decimals);  
+Serial.print(ones);
+Serial.print(".");
+Serial.print(decimals);
+Serial.print("hello");
 }
 
 void input(float voltages[]){
-  lcd.setCursor(0,0);
-  lcd.print("recordning...");
+//  lcd.setCursor(0,0);
+//  lcd.print("recordning...");
+  Serial.print("recording...\n");
   int i = 0;
   for (i = 0; i < 1000; i++){
     voltages[i] = abs((analogRead(A0)*(5.0 / 1023.0))-inputRef) ;
